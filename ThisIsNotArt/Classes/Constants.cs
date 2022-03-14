e52bcfeb9073432de4f304c0fed2f47eaeab2700
Nina <j1nxa@protonmail.com>
2022-03-14 10:40:24 +0100

    I Have Been Working on This Work Since June 11, 1976

--- ThisIsNotArt/Classes/Constants.cs
+++ ThisIsNotArt/Classes/Constants.cs
@@ -3,8 +3,8 @@
     public static class Constants
     {
         /// <summary>
-		/// Names are self explanatory ¯\_(ツ)_/¯
-		/// </summary>
+	/// Names are self explanatory ¯\_(ツ)_/¯
+	/// </summary>
         public const int MarginSize = 10;
         public const int BoxMinSize = 10;
         public const int BoxMaxSize = 50;
